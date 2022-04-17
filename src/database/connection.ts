0653084761beb7d98f0bc81fd708210801221629
lucasbbs <lucasbbs@live.fr>
2022-04-17 13:34:45 -0300

    Tryning to to get that done

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -4,13 +4,14 @@ import 'dotenv/config';
 
 const db = knex({
   client: 'pg',
-  connection: {
-    host: process.env.PG_HOST,
-    user: process.env.PG_USER,
-    password: process.env.PG_PASSWORD,
-    database: process.env.PG_DATABASE,
-  },
-  useNullAsDefault: true,
+  connection: process.env.DATABASE_URL,
+  // {
+  //   host: process.env.PG_HOST,
+  //   user: process.env.PG_USER,
+  //   password: process.env.PG_PASSWORD,
+  //   database: process.env.PG_DATABASE,
+  // },
+  // useNullAsDefault: true,
 });
 
 // const db = knex({
