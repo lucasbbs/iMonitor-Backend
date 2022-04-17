5107c5d7394a8a331c4a6706c7024e1fd7ff7aa8
lucasbbs <lucasbbs@live.fr>
2022-04-17 12:58:18 -0300

    Hopefully now this will be enough

--- knexfile.ts
+++ knexfile.ts
@@ -1,30 +1,30 @@
 const path = require('path');
 require('dotenv').config();
 
-// module.exports = {
-//   client: 'pg',
-//   connection: {
-//     host : process.env.PG_HOST,
-//     user : process.env.PG_USER,
-//     password : process.env.PG_PASSWORD,
-//     database : process.env.PG_DATABASE,
-//   },
-//   migrations: {
-//     directory: path.resolve(__dirname, 'src', 'database', 'migrations')
-//   },
-//   useNullAsDefault: true,
-// }
-
 module.exports = {
-  client: 'sqlite3',
+  client: 'pg',
   connection: {
-    filename: path.resolve(__dirname, 'src', 'database', 'database.sqlite'),
+    host: process.env.PG_HOST,
+    user: process.env.PG_USER,
+    password: process.env.PG_PASSWORD,
+    database: process.env.PG_DATABASE,
   },
   migrations: {
     directory: path.resolve(__dirname, 'src', 'database', 'migrations'),
   },
-  seeds: {
-    directory: path.resolve(__dirname, 'src', 'database', 'seeds'),
-  },
   useNullAsDefault: true,
 };
+
+// module.exports = {
+//   client: 'sqlite3',
+//   connection: {
+//     filename: path.resolve(__dirname, 'src', 'database', 'database.sqlite'),
+//   },
+//   migrations: {
+//     directory: path.resolve(__dirname, 'src', 'database', 'migrations'),
+//   },
+//   seeds: {
+//     directory: path.resolve(__dirname, 'src', 'database', 'seeds'),
+//   },
+//   useNullAsDefault: true,
+// };
